735d20856b6ad7797e3fba279a2fe8a9f09b5634
Jeff Labonte <grimsleepless@protonmail.com>
2020-10-26 22:47:01 -0400

    Add ubuntu install script

---
+++ install_scripts/Ubuntu/ubuntu_clean_install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt install apt-transport-https curl
+
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+sudo apt update
+
+sudo apt install brave-browser
+sudo apt install emacs neovim vim tmux neofetch zsh tlp git build-essential -y
