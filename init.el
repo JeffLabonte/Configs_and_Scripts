c006f9f3ddf015b047b80f3733449d4ff1275712
Jeff Labonte <grimsleepless@protonmail.com>
2021-01-27 13:11:36 -0500

    Update init.el

--- init.el
+++ init.el
@@ -13,11 +13,13 @@
     blacken
     elpy
     evil
+    flycheck
     ido
     neotree
     markdown-mode
     makefile-executor
     material-theme
+    plantuml-mode
     py-autopep8
     pyvenv
     yaml-mode
@@ -29,12 +31,23 @@
             (package-install package)))
       deps)
 
-;; ENable IDO
+;; Enable IDO
 (require 'ido)
 (ido-mode t)
 
 ;; Enable elpy
 (elpy-enable)
+
+(setq elpy-rpc-python-command "python3")
+(setq elpy-rpc-backend "jedi")
+(setq elpy-rpc-virtualenv-path 'current)
+
+;; Use IPython for REPL
+(setq python-shell-interpreter "jupyter"
+      python-shell-interpreter-args "console --simple-prompt"
+      python-shell-prompt-detect-failure-warning nil)
+(add-to-list 'python-shell-completion-native-disabled-interpreters
+             "jupyter")
 
 ;; Enable Neotree
 (require 'neotree)
@@ -47,17 +60,27 @@
 (require 'py-autopep8)
 (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
 
+;; Enable Flycheck
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
+
 ;; Enable yaml-mode
 (require 'yaml-mode)
 (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
+
+(require 'plantuml-mode)
+(add-to-list 'auto-mode-alist '("\\.puml\\'" . plantuml-mode))
 
 (setq inhibit-startup-message t)    ;; Hide the startup message
 (load-theme 'material t)            ;; Load material theme
 (global-linum-mode t)               ;; Enable line numbers globally
 
 (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
-(setq python-shell-interpreter "/usr/bin/python3")
-(setq elpy-rpc-python-command "python3")
+;; (setq python-shell-interpreter "/usr/bin/python3")
+
+(setq plantuml-jar-path "/home/jflabonte/.bin/plantuml.jar")
+(setq plantuml-default-exec-mode 'jar)
 
 ;; Move from buffer to buffer
 (global-set-key (kbd "C-x <up>") 'windmove-up)
@@ -80,7 +103,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (python-black evil))))
+ '(package-selected-packages (quote (plantuml-mode python-black evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -99,5 +122,3 @@
             (define-key evil-normal-state-local-map (kbd "p") 'neotree-previous-line)
             (define-key evil-normal-state-local-map (kbd "A") 'neotree-stretch-toggle)
             (define-key evil-normal-state-local-map (kbd "H") 'neotree-hidden-file-toggle)))
-
-
