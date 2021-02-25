f4c1acb2408c9e39d1e5c51493f8e0f98621a084
Jeff Labonte <grimsleepless@protonmail.com>
2021-02-24 23:43:39 -0500

    My Doom emacs configs

---
+++ .doom.d/custom.el
@@ -0,0 +1,18 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (mu4e-conversation smtpmail-multi mu4e-overview mu4e-views evil-mu4e)))
+ '(smtpmail-smtp-server "127.0.0.1" t)
+ '(smtpmail-smtp-service 1025 t)
+ '(smtpmail-smtp-user grimsleepless@protonmail\.com)
+ '(smtpmail-stream-type starttls))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
